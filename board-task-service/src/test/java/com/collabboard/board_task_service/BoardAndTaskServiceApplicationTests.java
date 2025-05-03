988bddc013c4ca6cf31147e8a9f413be493468b9
Assem Mohamed <assem08mohamed@gmail.com>
2025-05-04 00:31:59 +0300

    Refactoring

---
+++ board-task-service/src/test/java/com/collabboard/board_task_service/BoardAndTaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabboard.board_task_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardAndTaskServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
