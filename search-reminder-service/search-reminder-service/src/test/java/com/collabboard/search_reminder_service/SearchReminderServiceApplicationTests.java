29bc529c851682c134f5e4199b4faf6c4c298117
Assem Mohamed <assem08mohamed@gmail.com>
2025-05-03 23:04:43 +0300

    First Commit - Basic Structure

---
+++ search-reminder-service/search-reminder-service/src/test/java/com/collabboard/search_reminder_service/SearchReminderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabboard.search_reminder_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchReminderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
