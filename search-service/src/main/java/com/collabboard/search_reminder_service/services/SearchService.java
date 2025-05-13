afe14a55b5920e93a9466d3eb837a9499510d65d
BigMizo <mazentamer719@gmail.com>
2025-05-13 23:02:30 +0300

    search service

--- search-service/src/main/java/com/collabboard/search_reminder_service/services/SearchService.java
+++ search-service/src/main/java/com/collabboard/search_reminder_service/services/SearchService.java
@@ -1,9 +1,9 @@
 package com.collabboard.search_reminder_service.services;
 
-import com.collabboard.search_reminder_service.Clients.TaskClient;
-import com.collabboard.search_reminder_service.Clients.TaskDTO;
-import com.collabboard.search_reminder_service.Clients.UserClient;
-import com.collabboard.search_reminder_service.Clients.UserDTO;
+//import com.collabboard.search_reminder_service.Clients.TaskClient;
+//import com.collabboard.search_reminder_service.Clients.TaskDTO;
+//import com.collabboard.search_reminder_service.Clients.UserClient;
+//import com.collabboard.search_reminder_service.Clients.UserDTO;
 import com.collabboard.search_reminder_service.models.SearchContext;
 import com.collabboard.search_reminder_service.models.SearchRequest;
 import com.collabboard.search_reminder_service.models.TaskCache;
@@ -12,7 +12,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,14 +22,14 @@ public class SearchService {
     private static final String SEARCH_CACHE_PREFIX = "search:";
 
     @Autowired private SearchContext searchContext;
-    @Autowired private TaskClient taskClient;
-    @Autowired private UserClient userClient;
+//    @Autowired private TaskClient taskClient;
+//    @Autowired private UserClient userClient;
 
 
-    @Cacheable(value = "Tasks_SearchResult_Cache" , key = "#request.fullText" , condition = "#request.fullText != null")
-    public List<TaskDTO> searchTasks(SearchRequest request) {
-        List<TaskDTO> tasks = taskClient.getAllTasks(); // Fetch from Feign
-        return searchContext.executeStrategies(tasks, request);
-    }
+//    @Cacheable(value = "Tasks_SearchResult_Cache" , key = "#request.fullText" , condition = "#request.fullText != null")
+//    public List<TaskDTO> searchTasks(SearchRequest request) {
+//        List<TaskDTO> tasks = taskClient.getAllTasks(); // Fetch from Feign
+//        return searchContext.executeStrategies(tasks, request);
+//    }
 
 }
