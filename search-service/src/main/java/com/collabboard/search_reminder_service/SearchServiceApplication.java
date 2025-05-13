0abb14e4f7395df6abf500afeaba9ca911ece1b4
Sara-mkhaled <sara.khaled393@yahoo.com>
2025-05-13 23:27:36 +0300

    search service

--- search-service/src/main/java/com/collabboard/search_reminder_service/SearchServiceApplication.java
+++ search-service/src/main/java/com/collabboard/search_reminder_service/SearchServiceApplication.java
@@ -3,11 +3,12 @@ package com.collabboard.search_reminder_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients(basePackages = "com.collabboard.searchreminderservice.client")
-@SpringBootApplication
 @EnableCaching
+//@EnableFeignClients(basePackages = "com.collabboard.searchreminderservice.client")
+@SpringBootApplication
+
 public class SearchServiceApplication {
 
 	public static void main(String[] args) {
