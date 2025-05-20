9915b97cce899f544ce92f81a76c7779c7950d49
Abdelrahman0721 <121450555+Abdelrahman0721@users.noreply.github.com>
2025-05-20 22:27:57 +0300

    1

--- notification-service/src/main/java/com/collabboard/notification_service/rabbitmq/RabbitMQConfig.java
+++ notification-service/src/main/java/com/collabboard/notification_service/rabbitmq/RabbitMQConfig.java
@@ -1,34 +1,17 @@
 package com.collabboard.notification_service.rabbitmq;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
+
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
 
-    public static final String NOTIFICATION_QUEUE = "notification.queue";
-    public static final String EXCHANGE = "shared_exchange";
-    public static final String NOTIFICATION_ROUTING_KEY = "notification_routing_key";
+    public static final String NOTIFICATION_QUEUE = "notification_queue";
 
     @Bean
-    public Queue queue() {
-        return new Queue(NOTIFICATION_QUEUE,true);
-    }
-
-    @Bean
-    public TopicExchange exchange() {
-        return new TopicExchange(EXCHANGE);
-    }
-
-    @Bean
-    public Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder
-                .bind(queue)
-                .to(exchange)
-                .with(NOTIFICATION_ROUTING_KEY);
+    public Queue notificationQueue() {
+        return new Queue(NOTIFICATION_QUEUE, true); // durable
     }
 }
